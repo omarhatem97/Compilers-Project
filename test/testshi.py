f906bb2dce2fbda6cce94eedaa1eb9d14a38a1cb
omarhatem97 <omarhatem221@gmail.com>
2020-12-27 23:17:42 +0200

    change variable names and funcs to meaningful ones

---
+++ test/testshi.py
@@ -0,0 +1,10 @@
+from graphviz import Digraph
+
+
+
+dot = Digraph(comment='The Round Table')
+dot.node('A', 'King Arthur')
+dot.node('B', 'Sir Bedevere the Wise')
+dot.node('L', 'Sir Lancelot the Brave')
+dot.edge('B', 'L', constraint='false')
+dot.render('test-output/round-table.gv', view=True)
